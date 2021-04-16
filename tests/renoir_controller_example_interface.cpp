2d8282dce4472d5683d035759841ce95114d11e8
Olivier Stasse <olivier.stasse@laas.fr>
2021-04-16 08:49:44 +0200

    Add a more realistic test for the interface.

--- tests/renoir_controller_example_interface.cpp
+++ tests/renoir_controller_example_interface.cpp
@@ -26,43 +26,129 @@
 
 namespace renoir_controller {
 
+struct Sensors {
+  // Measured angle values at the motor side.
+  std::vector<double> motor_angle_;
+  // Measured angle at the joint side.
+  std::vector<double> joint_angle_;
+  // Measured or computed velocities.
+  std::vector<double> velocities_;
+  // Measured torques.
+  std::vector<double> torques_;
+  // Reconstructed orientation (from internal IMU).
+  std::vector<double> orientation_;
+  // Measured linear acceleration
+  std::vector<double> accelerometer_;
+  // Measured angular velocities
+  std::vector<double> gyrometer_;
+  // Measured 6D force sensors
+  std::vector<double> force_sensors_;
+  // Measured motor currents
+  std::vector<double> motor_currents_;
+  // Measured temperatures
+  std::vector<double> temperatures_;
+
+  std::size_t nbDofs_;
+  std::size_t nbForceSensors_;
+  std::size_t nbIMUs_;
+  
+  Sensors(std::size_t lNbDofs,
+          std::size_t lNbForceSensors,
+          std::size_t lNbIMUs)
+  {
+    nbDofs_ = lNbDofs;
+    nbForceSensors_ = lNbForceSensors;
+    nbIMUs_ = lNbIMUs;
+  }
+  
+  void init()
+  {
+    motor_angle_.resize(nbDofs_);
+    joint_angle_.resize(nbDofs_);
+    velocities_.resize(nbDofs_);
+    torques_.resize(nbDofs_);
+    orientation_.resize(3*nbIMUs_);
+    accelerometer_.resize(3*nbIMUs_);
+    gyrometer_.resize(3*nbIMUs_);
+    force_sensors_.resize(6*nbForceSensors_);
+    motor_currents_.resize(nbDofs_);
+    temperatures_.resize(nbDofs_);
+  }
+
+  void fillValue()
+  {
+    for(unsigned int i=0;i<nbDofs_;i++)
+    {
+      motor_angle_[i] = (double)i;
+      joint_angle_[i] = (double)(i+nbDofs_);
+      velocities_[i] = (double)(i+2*nbDofs_);
+      torques_[i] = (double)(i+3*nbDofs_);
+      force_sensors_[i] = (double)(i+4*nbDofs_);
+      motor_currents_[i] = (double)(i+5*nbDofs_);
+      temperatures_[i] = (double)(i+6*nbDofs_);
+    }
+    
+    for(unsigned int i=0;i<nbIMUs_;i++)
+    {
+      for (unsigned int j=0;j<3;j++)
+      {
+        orientation_[j] = (double)(j+i*nbIMUs_*3);
+        accelerometer_[j] = (double)(j+i*nbIMUs_*3);
+        gyrometer_[j] = (double)(j+i*nbIMUs_*3);
+      }
+    }    
+  }
+};
+
+
 class SimpleExternalInterface: public AbstractExternalInterface {
 public:
-  SimpleExternalInterface() {}
+
+  Sensors renoirSensorValues_;
+  std::vector<double> control_;
+  
+  SimpleExternalInterface():
+      renoirSensorValues_(30,4,1)
+  {
+    renoirSensorValues_.init();
+  }
 
   virtual ~SimpleExternalInterface() {}
 
+  void fillSensorValues(std::map<std::string, SensorValues> &sensorsIn)
+  {
+    renoirSensorValues_.fillValue();
+    sensorsIn["motor_angle"].setValues(renoirSensorValues_.motor_angle_);
+    sensorsIn["joint_angle"].setValues(renoirSensorValues_.joint_angle_);
+    sensorsIn["velocities"].setValues(renoirSensorValues_.velocities_);
+    sensorsIn["torques"].setValues(renoirSensorValues_.torques_);
+    sensorsIn["motor_currents"].setValues(renoirSensorValues_.motor_currents_);
+    sensorsIn["temperatures"].setValues(renoirSensorValues_.temperatures_);
+  }
+  
   virtual void
   setupSetSensors(std::map<std::string, SensorValues> &sensorsIn)
   {
-    std::cout << " setupSetSensors - Size of sensorsIn:"
-              << sensorsIn.size()
-              << std::endl;
+    fillSensorValues(sensorsIn);
   }
 
   virtual void
   nominalSetSensors(std::map<std::string, SensorValues> &sensorsIn)
   {
-    std::cout << " nominalSetSensors - Size of sensorsIn:"
-              << sensorsIn.size()
-              << std::endl;
+    fillSensorValues(sensorsIn);
   }
       
 
   virtual void
   cleanupSetSensors(std::map<std::string, SensorValues> &sensorsIn)
   {
-    std::cout << " cleanupSetSensors - Size of sensorsIn:"
-              << sensorsIn.size()
-              << std::endl;
+    fillSensorValues(sensorsIn);
   }
   
   virtual void
   getControl(std::map<std::string, ControlValues> &controlOut)
   {
-    std::cout << " getControl - Size of controlOut:"
-              << controlOut.size()
-              << std::endl;
+    controlOut["control"];
   }
 };
 } // namespace renoir_controller
@@ -70,11 +156,6 @@ public:
 extern "C" {
 renoir_controller::AbstractExternalInterface * createExternalInterface()
 {
-  /* renoir_controller::SimpleExternalInterface *aSimpExtInt =
-      new renoir_controller::SimpleExternalInterface();
-  
-  return dynamic_cast<renoir_controller::AbstractExternalInterface *>
-  (aSimpExtInt);*/
   return new renoir_controller::SimpleExternalInterface();
 }
 }
